1c04855f3664b000b3d7217db198c4dd08a9e84c
Di Jiao <v-jiaodi@microsoft.com>
2023-11-01 13:54:58 +0800

    todo-tf

--- Environments/Todoterraform/main.tf
+++ Environments/Todoterraform/main.tf
@@ -1,33 +1,20 @@
-
 terraform {
   required_providers {
     azurerm = {
-      version = "~>3.47.0"
       source  = "hashicorp/azurerm"
-    }
-    azurecaf = {
-      source  = "aztfmod/azurecaf"
-      version = "~>1.2.24"
+      version = ">= 3.0.0"
     }
   }
+
+  required_version = ">= 1.0.0"
 }
+
 provider "azurerm" {
   features {}
 
   skip_provider_registration = true
 }
 
-# ------------------------------------------------------------------------------------------------------
-
-# ------------------------------------------------------------------------------------------------------
-
-#locals {
-#  tags                         = { azd-env-name : var.environment_name }
-#  sha                          = base64encode(sha256("${var.environment_name}${var.location}))
-#  resource_token               = substr(replace(lower(local.sha), "[^A-Za-z0-9_]", ""), 0, 13)
-#  cosmos_connection_string_key = "AZURE-COSMOS-CONNECTION-STRING"
-#}
-variable "tags" { azd-env-name : var.environment_name }
 variable "resource_group_name" {}
 
 variable "resource_name" {}
@@ -47,24 +34,12 @@ resource "azurecaf_name" "plan_name" {
   random_length = 0
   clean_input   = true
 }
-variable "sku_name" {
-  description = "The SKU for the plan."
-  type        = string
-  default     = "B1"
-}
-
-variable "os_type" {
-  description = "The O/S type for the App Services to be hosted in this plan."
-  type        = string
-  default     = "Linux"
-}
 resource "azurerm_service_plan" "service_plan" {
   name                = azurecaf_name.plan_name.result
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = var.location
-  sku_name            = var.os_type
-  os_type             = var.sku_name
-  tags = var.tags
+  sku_name            = "B1"
+  os_type             = "Linux"
 }
 
 output "APPSERVICE_PLAN_ID" {
