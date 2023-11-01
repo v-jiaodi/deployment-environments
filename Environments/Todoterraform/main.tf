c26c0d275a5ec83b6ccc3e88a524d665bb151f93
Di Jiao <v-jiaodi@microsoft.com>
2023-11-01 10:49:48 +0800

    todo-tf

--- Environments/Todoterraform/main.tf
+++ Environments/Todoterraform/main.tf
@@ -1,94 +1,73 @@
-locals {
-  tags                         = { azd-env-name : var.environment_name }
-  sha                          = base64encode(sha256("${var.environment_name}${var.location}))
-  resource_token               = substr(replace(lower(local.sha), "[^A-Za-z0-9_]", ""), 0, 13)
-  cosmos_connection_string_key = "AZURE-COSMOS-CONNECTION-STRING"
+
+terraform {
+  required_providers {
+    azurerm = {
+      version = "~>3.47.0"
+      source  = "hashicorp/azurerm"
+    }
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "~>1.2.24"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+
+  skip_provider_registration = true
 }
 
+# ------------------------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------------------------
+
+#locals {
+#  tags                         = { azd-env-name : var.environment_name }
+#  sha                          = base64encode(sha256("${var.environment_name}${var.location}))
+#  resource_token               = substr(replace(lower(local.sha), "[^A-Za-z0-9_]", ""), 0, 13)
+#  cosmos_connection_string_key = "AZURE-COSMOS-CONNECTION-STRING"
+#}
+variable "tags" { azd-env-name : var.environment_name }
 variable "resource_group_name" {}
 
 variable "resource_name" {}
 
 variable "location" {}
-
+variable sha = base64encode(sha256("${var.environment_name}${var.location}))
+variable resource_token = substr(replace(lower(local.sha), "[^A-Za-z0-9_]", ""), 0, 13)
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 # ------------------------------------------------------------------------------------------------------
-# Deploy resource Group
+# Deploy service_plan
 # ------------------------------------------------------------------------------------------------------
-resource "azurecaf_name" "rg_name" {
-  name          = var.environment_name
-  resource_type = "azurerm_resource_group"
+resource "azurecaf_name" "plan_name" {
+  name          = var.resource_token
+  resource_type = "azurerm_app_service_plan"
   random_length = 0
   clean_input   = true
 }
-
-# ------------------------------------------------------------------------------------------------------
-# Deploy application insights
-# ------------------------------------------------------------------------------------------------------
-
-
-# ------------------------------------------------------------------------------------------------------
-# Deploy log analytics
-# ------------------------------------------------------------------------------------------------------
-
-# ------------------------------------------------------------------------------------------------------
-# Deploy key vault
-# ------------------------------------------------------------------------------------------------------
-module "keyvault" {
-  source                   = "./modules/keyvault"
-  location                 = var.location
-  rg_name                  = azurerm_resource_group.rg.name
-  tags                     = azurerm_resource_group.rg.tags
-  resource_token           = local.resource_token
+variable "sku_name" {
+  description = "The SKU for the plan."
+  type        = string
+  default     = "B1"
 }
 
-# ------------------------------------------------------------------------------------------------------
-# Deploy cosmos
-# ------------------------------------------------------------------------------------------------------
+variable "os_type" {
+  description = "The O/S type for the App Services to be hosted in this plan."
+  type        = string
+  default     = "Linux"
+}
+resource "azurerm_service_plan" "service_plan" {
+  name                = azurecaf_name.plan_name.result
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = var.location
+  sku_name            = var.os_type
+  os_type             = var.sku_name
+  tags = var.tags
+}
 
-# ------------------------------------------------------------------------------------------------------
-# Deploy app service plan
-# ------------------------------------------------------------------------------------------------------
-
-
-# ------------------------------------------------------------------------------------------------------
-# Deploy app service web app
-# ------------------------------------------------------------------------------------------------------
-
-
-# ------------------------------------------------------------------------------------------------------
-# Deploy app service api
-# ------------------------------------------------------------------------------------------------------
-
-
-# ------------------------------------------------------------------------------------------------------
-# Deploy app service apim
-# ------------------------------------------------------------------------------------------------------
-#module "apim" {
-#  count                     = var.useAPIM ? 1 : 0
-#  source                    = "./modules/apim"
-#  name                      = "apim-${local.resource_token}"
-#  location                  = var.location
-#  rg_name                   = azurerm_resource_group.rg.name
-# tags                      = merge(local.tags, { "azd-service-name" : var.environment_name })
-#  application_insights_name = module.applicationinsights.APPLICATIONINSIGHTS_NAME
-#  sku                       = "Consumption"
-#}
-
-# ------------------------------------------------------------------------------------------------------
-# Deploy app service apim-api
-# ------------------------------------------------------------------------------------------------------
-#module "apimApi" {
-#  count                    = var.useAPIM ? 1 : 0
-#  source                   = "./modules/apim-api"
-#  name                     = module.apim[0].APIM_SERVICE_NAME
-#  rg_name                  = azurerm_resource_group.rg.name
-#  web_front_end_url        = module.web.URI
-#  api_management_logger_id = module.apim[0].API_MANAGEMENT_LOGGER_ID
-#  api_name                 = "todo-api"
-#  api_display_name         = "Simple Todo API"
-#  api_path                 = "todo"
-#  api_backend_url          = module.api.URI
-#}
+output "APPSERVICE_PLAN_ID" {
+  value     = azurerm_service_plan.plan.id
+  sensitive = true
+}
