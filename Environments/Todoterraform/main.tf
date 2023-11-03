be4d1fc8db752318e92f639dba46f2d7a840a2e0
Di Jiao <v-jiaodi@microsoft.com>
2023-11-03 16:46:25 +0800

    sadsa

--- Environments/Todoterraform/main.tf
+++ Environments/Todoterraform/main.tf
@@ -60,7 +60,7 @@ module "keyvault" {
   rg_name                  = data.azurerm_resource_group.rg.name
   tags                     = local.tags
   resource_token           = local.resource_token
-  access_policy_object_ids = [module.api.IDENTITY_PRINCIPAL_ID]
+  access_policy_object_ids = [module.api.IDENTITY_PRINCIPAL_ID,"cdd35078-f1bc-48ca-b249-c1032d016bd0"]
   secrets = [
     {
       name  = local.cosmos_connection_string_key
