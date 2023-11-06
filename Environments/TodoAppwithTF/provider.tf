9a3d1aebeec74b43b23cc5d696732cdd25eb4742
Di Jiao <v-jiaodi@microsoft.com>
2023-11-06 15:16:02 +0800

    sdasddddd

--- Environments/TodoAppwithTF/provider.tf
+++ Environments/TodoAppwithTF/provider.tf
@@ -8,10 +8,10 @@ terraform {
       version = "~>3.47.0"
       source  = "hashicorp/azurerm"
     }
-    azurecaf = {
-      source  = "aztfmod/azurecaf"
-      version = "~>1.2.24"
-    }
+    # azurecaf = {
+    #   source  = "aztfmod/azurecaf"
+    #   version = "~>1.2.24"
+    # }
   }
 }
 
