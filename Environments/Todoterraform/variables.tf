06982193ef50cea5ca5093cb23a96d3a7e959155
Di Jiao <v-jiaodi@microsoft.com>
2023-11-06 17:38:54 +0800

    sdasddddd

--- Environments/Todoterraform/variables.tf
+++ Environments/Todoterraform/variables.tf
@@ -14,6 +14,13 @@ variable "principal_id" {
   default     = "e55fd960-0a37-471f-bc20-0e636d737258"
 }
 
+variable "apinode" {
+  description = "The Id of the azd service principal to add to deployed keyvault access policies"
+  type        = string
+  default     = "github.com/Azure-Samples/todo-python-mongo-terraform/tree/main/infra/modules/appservicepython"
+}
+
+
 variable "useAPIM" {
   description = "Flag to use Azure API Management to mediate the calls between the Web frontend and the backend API."
   type        = bool
